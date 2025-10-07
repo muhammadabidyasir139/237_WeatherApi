cb0dd77eede8b3dd1dd0129f4adfe34cd89042ad
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-10-07 11:13:49 +0700

    Mengambil data dari api

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 const API_KEY = "13c646f3661f451abae20558250710";
 
 document.getElementById("getCuaca").addEventListener("click", getCuaca);
+
+ function getCuaca() {
+        const input = document.getElementById("lokasiInput").value.trim();
+        const errorEl = document.getElementById("errorMessage");
+        const resultEl = document.getElementById("result");
+
+        errorEl.style.display = "none";
+        resultEl.style.display = "none";
+
+        if (!input) {
+          errorEl.textContent = "⚠️ Silakan masukkan lokasi!";
+          errorEl.style.display = "block";
+          return;
+        }
+
+        const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${encodeURIComponent(
+          input
+        )}&aqi=no`;
+
+         $.ajax({
+          url: url,
+          method: "GET",
+          success: function (data) {
+            // Isi data
+            document.getElementById("namaKota").textContent =
+              data.location.name || "–";
+            document.getElementById("negara").textContent =
+              data.location.country || "–";
+            document.getElementById("provinsi").textContent =
+              data.location.region || "–";
+            document.getElementById("latitude").textContent =
+              data.location.lat || "–";
+            document.getElementById("longitude").textContent =
+              data.location.lon || "–";
+            document.getElementById("suhu").textContent =
+              data.current.temp_c || "–";
+            document.getElementById("kelembaban").textContent =
+              data.current.humidity || "–";
+
+            resultEl.style.display = "block";
+          },
+
